006d431742c8c7334aa974a7c6e14b71cc84c033
starkhasan <alihasan226@gmail.com>
2021-05-02 10:50:08 +0530

    Fixed Issues

--- Recursion/Palindrome.java
+++ Recursion/Palindrome.java
@@ -11,12 +11,13 @@ public class Palindrome {
     // }
 
     public static boolean checkPalindrome(String str){
-        for(int i=0;i<str.length();i++){
+        for(int i=0;i<str.length()/2;i++){
             if(str.charAt(i) != str.charAt(str.length()-1-i))
                 return false;
         }
         return true;
     }
+
     public static void main(String[] args) {
         String st = "112233445566778899000000998877665544332211";
         if(checkPalindrome(st)){
