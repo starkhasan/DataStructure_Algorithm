626c413298ec35b9f6d199957a002a7b2b605992
starkhasan <alihasan226@gmail.com>
2021-06-10 02:33:38 +0530

    Modify Code

--- Tree/BinarySearchTree/Demo.java
+++ Tree/BinarySearchTree/Demo.java
@@ -1,10 +1,46 @@
 import java.util.*;
 import java.io.*;
-class Demo{
-    public static void main(String[] args) throws IOException{
+class Node{
+    Node left;
+    Node right;
+    int data;
+    Node(int data){
+        this.data = data;
+        left = right = null;
+    }
+}
+public class Demo{
+    static Node insert(Node root,int data){
+        if(root == null)
+            return new Node(data);
+        else{
+            if(data <= root.data)
+                root.left = insert(root.left,data);
+            else
+                root.right = insert(root.right,data);
+            return root;
+        }
+    }
+
+    static void inorder(Node focusNode){
+        if(focusNode!=null){
+            inorder(focusNode.left);
+            System.out.print(focusNode.data+" ");
+            inorder(focusNode.right);
+        }
+    }
+    public static void main(String[] args) throws FileNotFoundException{
         var file = new File("Input.txt");
         var scanner = new Scanner(file);
-        
+        Node root = null;
+        var input = "";
+        while(scanner.hasNext())
+            input = scanner.nextLine();
+        for (String string : input.split(" ")){
+            root = insert(root,Integer.parseInt(string));
+        }
+        System.out.println("\nInorder Traversal of Binary Search Tree : ");
+        inorder(root);
         scanner.close();
     }
 }
