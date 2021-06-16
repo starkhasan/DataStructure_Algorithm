149d1954b416cd2f715aa58cd4f773b96608613a
starkhasan <alihasan226@gmail.com>
2021-06-17 03:22:00 +0530

    Add and Modify Problem in LinkedList

--- LinkedLists/Demo.java
+++ LinkedLists/Demo.java
@@ -1,71 +1,11 @@
 import java.util.*;
-import java.io.*;
-class Node{
-    int data;
-    Node next;
-    Node(int data){
-        this.data = data;
-        next = null;
-    }
-}
 class Demo{
-    static Node insert(Node head,int data){
-        if(head == null)
-            return new Node(data);
-        else{
-            var current = head;
-            while(current.next!=null)
-                current = current.next;
-            current.next = new Node(data);
-            return head;
+    public static void main(String[] args) {
+        ArrayList<Integer>[] list = new ArrayList<Integer>[5];
+        for (int i = 0; i < 5; i++) {
+            list[i] = new ArrayList<Integer>();
         }
-    }
-
-    static void printLinkedList(Node head){
-        var temp = head;
-        while(temp!=null){
-            System.out.print(temp.data+" ");
-            temp = temp.next;
-        }
-    }
-
-    static Node makeCycle(Node head){
-        var temp = head;
-        while(temp.next!=null)
-            temp = temp.next;
-        temp.next = head;
-        return head;
-    }
-
-    static boolean hasCycle(Node head){
-        var slow = head.next;
-        var fast = head.next.next;
-        while(slow != null && fast!= null && fast.next!= null){
-            if(slow == fast)
-                return true;    
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-        return false;
-    }
-
-    public static void main(String[] args) throws FileNotFoundException{
-        var file = new File("Input.txt");
-        var scanner = new Scanner(file);
-        var input = "";
-        Node head = null;
-        while(scanner.hasNext())
-            input = scanner.nextLine();
-        for (String string : input.split(" ")){
-            head = insert(head,Integer.parseInt(string));
-        }       
-        printLinkedList(head);
-        head = makeCycle(head);
-        var result = hasCycle(head);
-        if(result)
-            System.out.println("Yes Loop is Present");
-        else
-            System.out.println("No! Loop not present");
-        scanner.close();
+        list[0].add(5);
+        System.out.println(list[0].get(0));
     }
 }
