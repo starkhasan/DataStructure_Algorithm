685426953190e781f32b58d998b7e57f0ef9b48e
starkhasan <alihasan226@gmail.com>
2021-07-10 21:28:34 +0530

    Updated

--- Array/Demo.java
+++ Array/Demo.java
@@ -1,17 +1,9 @@
 import java.util.*;
 import java.io.*;
 class Demo{
-    
     public static void main(String[] args) throws FileNotFoundException{
         var file = new File("Input.txt");
         var scanner = new Scanner(file);
-        var input = "";
-        while(scanner.hasNext())
-            input = scanner.nextLine();
-        var ar = new int[input.split(" ").length];
-        for (int i = 0; i < ar.length; i++) {
-            ar[i] = Integer.parseInt(input.split(" ")[i]);
-        }
-        scanner.close();       
+        scanner.close();
     }
 }
